ea3111dcd0b4df31f1174ccb4a6d608fa6968152
Ed-2 <167571531+Ed-2@users.noreply.github.com>
2024-04-19 23:23:30 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ pygame.init()
 
 screen = pygame.display.set_mode((600, 600))
 pygame.display.set_caption('Game')
+
+def draw1(c1,c2):
+  pygame.draw.rect(screen, (99,99,99), pygame.Rect(c1, (c2[0]-c1[0], c2[1]-c1[1])))
+
 while True:
   for event in pygame.event.get():
     if event.type == QUIT:
